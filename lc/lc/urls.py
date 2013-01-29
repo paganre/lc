65d22e4778f26d95985dcc77d1cf613a9eae2d64
Sina <sina@192.168.1.8>
2013-01-29 02:34:22 -0800

    Naive implementations of Login / Register and Logout

--- lc/lc/urls.py
+++ lc/lc/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'webapp.views.home', name='home'),
+    url(r'^logout/$', 'webapp.views.logoutUser'),
     # url(r'^lc/', include('lc.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- lc/webapp/views.py
+++ lc/webapp/views.py
@@ -1,5 +1,51 @@
+from django.views.decorators.csrf import csrf_protect
+from django.contrib import auth
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect, HttpResponse
 
+# Common site request forgery protection risk
+# Request is obtained from the login.html via POST
+# I am using both CSRF Middleware and csrf_protect() for extra security
+# More info: https://docs.djangoproject.com/en/dev/ref/contrib/csrf/
+@csrf_protect
 def home(request):
-    return render_to_response('home.html',{"session": request.session},context_instance=RequestContext(request))
+    loginfail = False
+    if request.method == 'POST':
+        isRegister = request.POST.get('isRegister', '')
+        if isRegister == "True":
+            rnickname = request.POST.get('rnickname','')
+            rpassword = request.POST.get('rpassword','')
+            remail = request.POST.get('remail','')
+            if rnickname and rpassword:
+                #-------- -------- -------
+                #Uncomment the body of the if-clause
+                #To activate registration
+                #-------- -------- -------
+                #user = User.objects.create_user(username = rnickname,
+                #                                password = rpassword,
+                #                                email = remail)
+                #user.is_staff = False
+                #user.save()
+                #auth.login(request, user)
+                return HttpResponseRedirect("")
+            else:
+                return HttpResponseRedirect("")
+        else:
+            nickname = request.POST.get('nickname', '')
+            password = request.POST.get('password', '')
+            user = auth.authenticate(username=nickname, password=password)
+            if user is not None and user.is_active:
+                auth.login(request, user);
+                return HttpResponseRedirect("")
+            else:
+                loginfail = True
+                return render_to_response('home.html',{"user": request.user, "loginfail": loginfail},context_instance=RequestContext(request))
+    else:
+        return render_to_response('home.html',{"user": request.user, "loginfail": loginfail},context_instance=RequestContext(request))
+
+def logoutUser(request):
+    leaving_user = request.user.username
+    auth.logout(request)
+    return HttpResponse("Bye %s!" % leaving_user)
